47452278fdfcf9e959b11a70193d45bbc51530d4
Emir Aydın <84669393+ibrahimemiraydin@users.noreply.github.com>
2024-09-22 02:01:27 +0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Fare pozisyonunu güncelleyen fonksiyon
-function updateMousePosition(event) {
-    const x = event.clientX;
-    const y = event.clientY;
-    const positionDiv = document.getElementById('position');
-    positionDiv.textContent = `X: ${x}, Y: ${y}`;
-}
 
-// Fare hareketini dinle
-document.addEventListener('mousemove', updateMousePosition);
